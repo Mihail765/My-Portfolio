ecd59224a11e59d2a0c56cc1af2e046234b7c90a
Mihail Miloshevski <milosevskimihail123@gmail.com>
2025-12-07 14:17:28 +0100

    Refactor particle and animation setup in index.js
    emmail id changed

--- index.js
+++ index.js
@@ -188,5 +188,6 @@ function sendMail(){
         subject: document.getElementById("subject").value,
         message: document.getElementById("message").value,
     }
-    emailjs.send("service_c5df02v", "template_dq2n1mf", parms).then(alert("Email Sent!!!"));
+    emailjs.send("service_ostdbee", "template_dq2n1mf", parms).then(alert("Email Sent!!!"));
 }
+
